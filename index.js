1ad5a9d4a36ea5a78a77b328cfd3983f705ee0c5
Earl <earlmunyua96@gmail.com>
2023-10-11 15:25:15 +0300

    Modification

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 main.remove()
 
 const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory")
+newHeader.id = "victory"
 
-newHeader.textContent = "Earl is the champion"
+newHeader.innerHTML= "Munyua is the champion"
 
